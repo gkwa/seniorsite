af698be33847d708d8cd095bd310b48eeb22acd6
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-17 18:24:55 -0700

    Remove db-sg and alb-sg

--- lib/constructs/SecurityGroup.ts
+++ lib/constructs/SecurityGroup.ts
@@ -5,27 +5,10 @@ interface SGProps { vpc: CfnVPC }
 
 export class SecurityGroup extends Construct {
     public readonly web: CfnSecurityGroup
-    public readonly db: CfnSecurityGroup
-    public readonly alb: CfnSecurityGroup
 
     constructor(scope: Construct, id: string, props: SGProps) {
         super(scope, id)
         const vpcId = props.vpc.ref
-
-        // Load Balancer Security Group
-        this.alb = new CfnSecurityGroup(this, `alb-sg`, {
-            vpcId,
-            groupDescription: `alb-sg`,
-            groupName: `alb-sg`,
-            tags: [{ key: "Name", value: `alb-sg` }],
-            securityGroupIngress: [{
-                cidrIp: '0.0.0.0/0',
-                description: 'Allow HTTP access from the internet',
-                ipProtocol: 'tcp',
-                fromPort: 80,
-                toPort: 80
-            }]
-        })
 
         // Web Security Group
         this.web = new CfnSecurityGroup(this, `web-sg`, {
@@ -51,21 +34,6 @@ export class SecurityGroup extends Construct {
                 ipProtocol: 'tcp',
                 fromPort: 22,
                 toPort: 22
-            }]
-        })
-
-        // DB Security Group
-        this.db = new CfnSecurityGroup(this, `db-sg`, {
-            vpcId,
-            groupDescription: `db-sg`,
-            groupName: `db-sg`,
-            tags: [{ key: "Name", value: `db-sg` }],
-            securityGroupIngress: [{
-                sourceSecurityGroupId: this.web.attrGroupId,
-                description: 'Allow SQL access from Web security group',
-                ipProtocol: 'tcp',
-                fromPort: 3306,
-                toPort: 3306
             }]
         })
     }
