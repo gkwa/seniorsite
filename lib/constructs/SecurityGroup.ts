8956b6d3d090581cc8c2473e16b63582cc897a20
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-21 11:16:41 -0700

    failing

--- lib/constructs/SecurityGroup.ts
+++ lib/constructs/SecurityGroup.ts
@@ -34,7 +34,6 @@ export class SecurityGroup extends Construct {
             }
         ];
 
-        cfnSecurityGroup.securityGroupIngress = securityGroupIngress
         this.web = cfnSecurityGroup
     }
 }
