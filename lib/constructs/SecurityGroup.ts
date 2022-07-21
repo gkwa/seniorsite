79c65039ec653fd0f501f878545b1234d2a54264
Taylor Monacelli <taylormonacelli@gmail.com>
2022-07-21 10:34:23 -0700

    ec2.CfnSecurityGroup.IngressProperty

--- lib/constructs/SecurityGroup.ts
+++ lib/constructs/SecurityGroup.ts
@@ -1,5 +1,6 @@
 import { CfnSecurityGroup, CfnVPC, Vpc } from "aws-cdk-lib/aws-ec2";
 import { Construct } from "constructs";
+import { aws_ec2 as ec2 } from 'aws-cdk-lib';
 
 interface SGProps { vpc: CfnVPC }
 
@@ -10,31 +11,28 @@ export class SecurityGroup extends Construct {
         super(scope, id)
         const vpcId = props.vpc.ref
 
-        // Web Security Group
-        this.web = new CfnSecurityGroup(this, `web-sg`, {
-            vpcId,
+        const cfnSecurityGroup = new ec2.CfnSecurityGroup(this, 'MyCfnSecurityGroup', {
             groupDescription: `web-sg`,
             groupName: `web-sg`,
             tags: [{ key: "Name", value: `web-sg` }],
-            securityGroupIngress: [{
-                cidrIp: '0.0.0.0/0',
-                description: 'Allow HTTP access from the internet',
-                ipProtocol: 'tcp',
-                fromPort: 80,
-                toPort: 80
-            }, {
-                cidrIp: '0.0.0.0/0',
-                description: 'Allow HTTPS access from the internet',
-                ipProtocol: 'tcp',
-                fromPort: 443,
-                toPort: 443
-            }, {
-                cidrIp: '0.0.0.0/0',
-                description: 'Allow SSH access from the internet',
-                ipProtocol: 'tcp',
-                fromPort: 22,
-                toPort: 22
-            }]
-        })
+            vpcId
+        });
+
+        const webIngressProperty: ec2.CfnSecurityGroup.IngressProperty = {
+            ipProtocol: 'tcp',
+            cidrIp: '0.0.0.0/0',
+            description: 'Allow HTTP access from the internet',
+            fromPort: 80,
+            toPort: 80,
+            sourceSecurityGroupId: cfnSecurityGroup.ref
+        };
+
+        const webIngressProperty2: ec2.CfnSecurityGroup.IngressProperty = {
+            ipProtocol: '-1',
+            sourceSecurityGroupName: cfnSecurityGroup.groupName,
+            description: 'all traffic',
+        };
+
+        this.web = cfnSecurityGroup
     }
 }
