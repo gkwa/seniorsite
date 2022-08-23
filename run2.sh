c730a56bb54d7af522f57755f42e9ce7bebbd6de
Taylor Monacelli <taylormonacelli@gmail.com>
2022-08-23 00:20:56 -0700

    run2

---
+++ run2.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cdk synth
+yq e -P cdk.out/NetworkStack.template.json >cdk.out/NetworkStack.template.yaml
+
+yq --inplace '
+    del(.Rules) |
+    del(.Conditions.CDKMetadataAvailable) |
+    del(.. | select(has("CDKMetadata")).CDKMetadata) |
+    del(.. | select(has("Metadata")).Metadata) |
+    del(.. | select(has("BootstrapVersion")).BootstrapVersion)
+' cdk.out/NetworkStack.template.yaml
+
+git diff --color $path
