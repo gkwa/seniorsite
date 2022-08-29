e197f13d23dc5c123d84646a0e55805d3046fd2b
Taylor Monacelli <taylormonacelli@gmail.com>
2022-08-29 09:27:08 -0700

    lessen yq

--- yqclean.sh
+++ yqclean.sh
@@ -4,18 +4,7 @@ set -e
 
 yq --inplace '
     del(.. | select(tag == "!!map" and length == 0)) |
-    del(.Rules) |
     del(.[].[].Metadata) |
-    del(.Conditions.CDKMetadataAvailable) |
     del(.. | select(has("CDKMetadata")).CDKMetadata) |
-    del(.. | select(has("BootstrapVersion")).BootstrapVersion) |
-    del(.[].vpcsbxcdivpcFEE27BEE) |
-    del(.[].subnetscdiAsubnetF37012F5) |
-    del(.[].subnetscdiBsubnet1A3B9F51) |
-    del(.[].IGWigwsbxcdi7C0EA41C) |
-    del(.[].IGWvpcgwsbxcdiDF298982) |
-    del(.[].rtablescdiArtableA59AF960) |
-    del(.[].rtablescdiBsrta63414513) |
-    del(.[].rtablescdiArouteEE2A88FE) |
-    del(.[].rtablescdiAsrtaA81E8116)
+    del(.. | select(has("BootstrapVersion")).BootstrapVersion)
 ' cdk.out/NetworkStack.template.yaml
